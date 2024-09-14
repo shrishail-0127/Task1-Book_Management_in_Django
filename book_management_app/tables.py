3cc6ff8cf9fc6c05c0a8377f90120999465fd0df
shrishil-0127 <shriiipj420@gmail.com>
2024-09-14 10:17:13 +0530

    made some small changes

---
+++ book_management_app/tables.py
@@ -0,0 +1,8 @@
+import django_tables2 as tables
+from .models import Book
+
+
+class  BookTable(tables.Table):
+    model = Book
+    # template_name = 'book_management_app/book_list.html'
+    # fields = ['title','file','uploaded_date']
